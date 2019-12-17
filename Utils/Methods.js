c81e1c53512d2b37ad648ab74dc354593ce0295c
jagdish_o2onelabs <JDchauhan@github.com>
2019-12-17 21:07:32 +0530

    random number + string jumble method

--- Utils/Methods.js
+++ Utils/Methods.js
@@ -102,3 +102,31 @@ module.exports.wordOfDay = async () => {
         console.log(err)
     }
 }
+
+// Random no generator
+const randomNum = (size) => {
+    return Math.ceil(Math.random() * size)
+}
+
+// jumbling string (@source Stackoverflow )
+// eslint-disable-next-line no-unused-vars
+const jumbled = (string) => {
+    if (string.length < 2) return string // This is our break condition
+
+    var permutations = [] // This array will hold our permutations
+    for (var i = 0; i < string.length; i++) {
+        var char = string[i]
+
+        // Cause we don't want any duplicates:
+        if (string.indexOf(char) !== i) { // if char was used already
+            continue// skip it this time
+        }
+
+        var remainingString = string.slice(0, i) + string.slice(i + 1, string.length) // Note: you can concat Strings via '+' in JS
+
+        for (var subPermutation of jumbled(remainingString)) {
+            permutations.push(char + subPermutation)
+        }
+    }
+    return permutations
+}
